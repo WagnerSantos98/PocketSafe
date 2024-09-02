6528cabfe54c25f7e964dad998f2dd996f385498
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-09-02 17:15:11 -0300

    feat: implementação de controller para renda

--- ws/src/controllers/renda.js
+++ ws/src/controllers/renda.js
@@ -13,8 +13,37 @@ exports.addRenda = async (req, res) => {
 
     try{
         //Validação
-        if(!titulo || !categoria || !descricao || !data)
-    }catch(error){
+        if(!titulo || !categoria || !descricao || !data){
+            return res.status(400).json({error: 'Todos os campos são obrigatórios'});
+        }
+        if(quantia <= 0 || !quantia === 'number'){
+            return res.status(400).json({error: 'A quantia deve ser um número positivo'});
+        }
 
+        await renda.save();
+        res.status(201).json({msg: 'Renda adicionada com sucesso'});
+    }catch(error){
+        res.status(500).json({error: 'Ocorreu um erro interno'});
+    }
+}
+
+exports.getRendimentos = async (req, res) => {
+    try{   
+        const rendimentos = await  RendaSchemaSchema.find().sort({ createdAt: -1 })
+        res.status(200).json(rendimentos);
+    }catch(error){
+        res.status(500).json({msg: 'Falha ao buscar renda'});
+    }
+}
+
+exports.deleteRenda = async (req, res) => {
+    try{
+        const {id} = req.params;
+        RendaSchemaSchema.findByIdAndDelete(id)
+            .then((renda) => {
+                res.status(200).json({msg: 'Renda excluída com sucesso'});
+            })
+    }catch(error){
+        res.status(500).json({msg: 'Falha ao excluir renda'});
     }
 }
