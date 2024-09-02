bb616635578dce570dfeb6fcccc38c394c2c17dd
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-09-02 16:47:00 -0300

    build: instalação dos pacotes npm

---
+++ ws/src/database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const db = async () => {
+    try{
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_URL)
+
+        console.log('DB Conectado')
+    }catch(error){
+        console.log('DB Erro na Conexão');
+    }
+}
+
+module.exports = db;
